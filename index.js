dbc97a7ff5caef041655f8cd00f4227aa57aabd6
nndre <flav.varg@gmail.com>
2024-12-22 14:44:20 -0500

    Integrate OpenTelemetry logging with Winston in index.js

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-import winston from "winston";
+const logsAPI = require('@opentelemetry/api-logs');
+const {
+    LoggerProvider,
+    SimpleLogRecordProcessor,
+    ConsoleLogRecordExporter,
+} = require('@opentelemetry/sdk-logs');
+const { OpenTelemetryTransportV3 } = require('@opentelemetry/winston-transport');
+const winston = require('winston');
+// To start a logger, you first need to initialize the Logger provider.
+const loggerProvider = new LoggerProvider();
+// Add a processor to export log record
+loggerProvider.addLogRecordProcessor(
+    new SimpleLogRecordProcessor(new ConsoleLogRecordExporter())
+);
+logsAPI.logs.setGlobalLoggerProvider(loggerProvider);
 
 const logger = winston.createLogger({
-  transports: new winston.transports.Console(),
+  level: 'info',
+  transports: [
+    new winston.transports.Console(),
+    new OpenTelemetryTransportV3()
+  ]
 });
+
 export const handler = async (event) => {
     // TODO implement
     const response = {
