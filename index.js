5d0d71969cdac2d8354d49b8b1ca1d5979169c12
nndre <flav.varg@gmail.com>
2024-12-22 13:20:39 -0500

    Add .gitignore, package-lock.json, and configure winston logger in index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+import winston from "winston";
+
+const logger = winston.createLogger();
 exports.handler = async (event) => {
     // TODO implement
     const response = {
       statusCode: 200,
       body: JSON.stringify('Hello from home!'),
     };
+    logger.info("Hello from home! correctly sended");
     return response;
   };
   
