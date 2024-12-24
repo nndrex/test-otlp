b3bb9e5dd9a516fd2119cc766dfa6b331d7e82ef
nndre <flav.varg@gmail.com>
2024-12-24 10:59:22 -0500

    Add OpenTelemetry metrics to handler for execution time tracking

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const {
     SimpleLogRecordProcessor,
     ConsoleLogRecordExporter,
 } = require('@opentelemetry/sdk-logs');
+const {metrics, ValueType} = require('@opentelemetry/api');
 const { OpenTelemetryTransportV3 } = require('@opentelemetry/winston-transport');
 const winston = require('winston');
 // To start a logger, you first need to initialize the Logger provider.
@@ -22,13 +23,23 @@ const logger = winston.createLogger({
   ]
 });
 
+const meter =metrics.getMeter('example-meter');
 exports.handler = async (event) => {
+  const histogram = meter.createHistogram('example-histogram',{
+    description: 'Example histogram',
+    unit:'miliseconds',
+    valueType:ValueType.INT
+  });
     // TODO implement
+    const startTime = new Date().getTime();
     const response = {
       statusCode: 200,
       body: JSON.stringify('Hello from home!'),
     };
     logger.info("Hello from home! correctly sended");
+    const endTime = new Date().getTime();
+    const executionTime = endTime - startTime;
+    histogram.record(executionTime);
     return response;
   };
   
