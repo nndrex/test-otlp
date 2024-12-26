45c4f20e557d78a4b4952f2005eb36d295c60d09
nndre <flav.varg@gmail.com>
2024-12-26 10:52:09 -0500

    Refactor logging in index.js to include greetings function and add logic.js for additional functionality

--- index.js
+++ index.js
@@ -1,45 +1,66 @@
-const logsAPI = require('@opentelemetry/api-logs');
+const logsAPI = require("@opentelemetry/api-logs");
 const {
-    LoggerProvider,
-    SimpleLogRecordProcessor,
-    ConsoleLogRecordExporter,
-} = require('@opentelemetry/sdk-logs');
-const {metrics, ValueType} = require('@opentelemetry/api');
-const { OpenTelemetryTransportV3 } = require('@opentelemetry/winston-transport');
-const winston = require('winston');
+  LoggerProvider,
+  SimpleLogRecordProcessor,
+  ConsoleLogRecordExporter,
+} = require("@opentelemetry/sdk-logs");
+const { metrics, ValueType } = require("@opentelemetry/api");
+const {
+  OpenTelemetryTransportV3,
+} = require("@opentelemetry/winston-transport");
+const winston = require("winston");
 // To start a logger, you first need to initialize the Logger provider.
 const loggerProvider = new LoggerProvider();
 // Add a processor to export log record
 loggerProvider.addLogRecordProcessor(
-    new SimpleLogRecordProcessor(new ConsoleLogRecordExporter())
+  new SimpleLogRecordProcessor(new ConsoleLogRecordExporter())
 );
 logsAPI.logs.setGlobalLoggerProvider(loggerProvider);
 
 const logger = winston.createLogger({
-  level: 'info',
+  level: "info",
   transports: [
     new winston.transports.Console(),
-    new OpenTelemetryTransportV3()
-  ]
+    new OpenTelemetryTransportV3(),
+  ],
 });
-
-const meter =metrics.getMeter('example-meter');
-exports.handler = async (event) => {
-  const histogram = meter.createHistogram('example-histogram',{
-    description: 'Example histogram',
-    unit:'miliseconds',
-    valueType:ValueType.INT
-  });
-    // TODO implement
-    const startTime = new Date().getTime();
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify('Hello from home!'),
-    };
-    logger.info("Hello from home! correctly sended");
-    const endTime = new Date().getTime();
-    const executionTime = endTime - startTime;
-    histogram.record(executionTime);
-    return response;
+async function greetings(context) {
+  const startTime = new Date().getTime();
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify("Hello from greetings!"),
   };
-  
+  logger.info(context);
+  const endTime = new Date().getTime();
+  const executionTime = endTime - startTime;
+  histogram.record(executionTime);
+  return response;
+}
+
+const meter = metrics.getMeter("example-meter");
+exports.handler = async (event, context) => {
+  const histogram = meter.createHistogram("example-histogram", {
+    description: "Example histogram",
+    unit: "miliseconds",
+    valueType: ValueType.INT,
+  });
+  // TODO implement
+  switch (event) {
+    case "greetings":
+      await greetings(context);
+      break;
+
+    default:
+      const startTime = new Date().getTime();
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify("Hello from home!"),
+      };
+      logger.info("Hello from home! correctly sended");
+      const endTime = new Date().getTime();
+      const executionTime = endTime - startTime;
+      histogram.record(executionTime);
+      return response;
+      break;
+  }
+};
